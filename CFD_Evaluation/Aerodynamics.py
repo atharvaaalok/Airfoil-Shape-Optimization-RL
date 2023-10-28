9dfd1c17cc42a7eb93c20e40b4b47666f4b22158
atharvaaalok <atharvaaalok@gmail.com>
2023-10-28 01:42:56 -0700

    Added Xfoil code
    Added python interface to Xfoil. Calls xfoil and runs the appropriate commands to return lift-to-drag ratio and other aerodynamic properties.

---
+++ CFD_Evaluation/Aerodynamics.py
@@ -0,0 +1,89 @@
+import subprocess
+import numpy as np
+
+
+class Airfoil:
+    def __init__(self, airfoil_coordinates, airfoil_name):
+        self.coordinates = np.array(airfoil_coordinates)
+        self.name = airfoil_name
+    
+    def get_aerodynamic_properties(self, Re, angle_of_attack = 0):
+        aerodynamic_properties = CFD(self, Re, angle_of_attack)
+        return aerodynamic_properties
+
+    def get_L_by_D(self, Re, angle_of_attack = 0):
+        aerodynamic_properties = self.get_aerodynamic_properties(Re, angle_of_attack)
+        if aerodynamic_properties is None:
+            return None
+        return aerodynamic_properties['CL'] / aerodynamic_properties['CD']
+
+    def get_CL(self, Re, angle_of_attack = 0):
+        aerodynamic_properties = self.get_aerodynamic_properties(Re, angle_of_attack)
+        if aerodynamic_properties is None:
+            return None
+        return aerodynamic_properties['CL']
+
+    def get_CD(self, Re, angle_of_attack = 0):
+        aerodynamic_properties = self.get_aerodynamic_properties(Re, angle_of_attack)
+        if aerodynamic_properties is None:
+            return None
+        return aerodynamic_properties['CD']
+
+
+def CFD(airfoil, Re, angle_of_attack = 0):
+
+    # Make airfoil coordinate data file
+    airfoil_coord_filename = 'Airfoil_Coordinates.dat'
+    np.savetxt(airfoil_coord_filename, airfoil.coordinates, delimiter = ',')
+
+    # Start Xfoil
+    xfoil = subprocess.Popen('xfoil.exe', stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+
+    # Set CFD evaluation parameters
+    panel_count = 450
+    LE_TE_panel_density_ratio = 0.5
+    Reynolds_num = Re
+    max_iter_count = 1000
+
+    # Define the sequence of commands to execute to calculate the aerodynamic properties
+    command_list = ['PLOP\n',                               # Go to plotting options menu
+                    'G\n',                                  # Switch off graphical display 
+                    '\n',
+                    f'load {airfoil_coord_filename}\n',
+                    f'{airfoil.name}\n',
+                    'PPAR\n',                               # go to panel menu
+                    f'n {panel_count}\n',                   # set panel count
+                    f't {LE_TE_panel_density_ratio}\n',     # set Leading Edge to Trailing Edge panel density
+                    '\n',
+                    '\n',
+                    'OPER\n',                               # Go to operations menu to run the simulation
+                    'visc\n'
+                    f'{Reynolds_num}\n'
+                    f'iter {max_iter_count}\n'
+                    'PACC\n'                                # Turn on polar accumulation
+                    '\n',
+                    '\n',
+                    f'alfa {angle_of_attack}\n',
+                    'PLIS\n',                               # List the polar values
+                    '\n',
+                    'QUIT\n'
+                    ]
+    
+    # Execute the commands and close Xfoil
+    xfoil.stdin.write(''.join(command_list))
+    xfoil.stdin.flush()
+    xfoil.stdin.close()
+    
+    # Get the outputs from xfoil
+    xfoil_stdout, xfoil_stderr = xfoil.communicate()
+
+    # Extract the aerodynamic properties
+    coefficients = xfoil_stdout.splitlines()[-4].split()
+
+    # Check if the values are numbers, if not, then it did not converge and return None
+    try:
+        aerodynamic_properties = {'CL': float(coefficients[1]), 'CD': float(coefficients[2])}
+    except:
+        aerodynamic_properties = None
+
+    return aerodynamic_properties
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Airfoil Shape Optimization using Reinforcement Learning
 
-Stay tuned...
+## Motivation
+Aircraft design methods used today to determine shape, structure and size begin by taking features from similar aircraft that have been built before. Since this data comes from already built aircraft, it often leads to configurations being stuck in a sub-optimal design space. Since the design space is huge, it is extremely difficult for a human to explore or even guess the initial design. A revolution in aircraft design can therefore perhaps be brought about by machine learning methods which can ‘intelligently’ search through the design space to reach globally optimal configurations. Aircraft design is a tremendously complex process, therefore in this study we begin by exploring methods to design optimal airfoils, which are fundamental shapes that underpin aircraft wing design, imparting them their aerodynamic properties like lift and drag. A measure of an airfoil's quality is its lift-to-drag ratio, which only depends on its shape. In this study, we work towards reaching the optimal shape that achieves the highest lift-to-drag ratio.
+
+
+## Method
+The central part of our method is a reinforcement learning agent whose goal is to achieve an airfoil shape that maximizes the lift-to-drag ratio (its reward function). The RL agent holds a shape, which can be evaluated by a Computational Fluid Dynamics (CFD) solver (its environment) to give back its lift-to-drag ratio. The agent makes sequential changes to the shape (its actions) until an optimal shape is reached. The goal of the agent is to learn a policy that intelligently makes changes to efficiently reach the best design. This is represented in the figure below:
+
+
+<p align="center">
+  <img src="Resources/RL_Architecture.png">
+</p>
+
+## Contributors
+- Parth Prashant Lathi
+- Meenal Gupta
+- Atharva Aalok
+
+## References
+- Viquerat, Jonathan, et al. "Direct shape optimization through deep reinforcement learning." Journal of Computational Physics 428 (2021): 110080.
+- Dussauge, Thomas P., et al. "A reinforcement learning approach to airfoil shape optimization." Scientific Reports 13.1 (2023): 9753.
